05a5c3fcec9c643a17af8bcc51becb917e6c1457
anotherdeveloperonteam <dariuszpelka71@wp.pl>
2019-09-15 21:18:25 +0200

    Adding database objects

---
+++ DariuszPelka.TripManagementSystem.Database/CustomerTripAssignment.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CustomerTripAssignment]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [TripId] INT NOT NULL, 
+    [CustomerId] INT NOT NULL, 
+    CONSTRAINT [FK_CustomerTripAssignment_ToTrip] FOREIGN KEY ([TripId]) REFERENCES [Trip]([Id]), 
+    CONSTRAINT [FK_CustomerTripAssignment_ToCustomer] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([Id]) 
+)
---
+++ DariuszPelka.TripManagementSystem.Database/Trip.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Trip]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Destination] NVARCHAR(250) NULL, 
+    [ArrivalDate] DATETIME NULL
+)
